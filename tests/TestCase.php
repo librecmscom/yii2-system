160163c6abaea318a72ddae34f156545baaa70a2
xutl <79109681@qq.com>
2016-10-09 18:06:35 +0800

    单元测试

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -35,6 +35,22 @@ abstract class TestCase extends \PHPUnit_Framework_TestCase
         ], $config));
     }
 
+    protected function mockWebApplication($config = [], $appClass = '\yii\web\Application')
+    {
+        new $appClass(ArrayHelper::merge([
+            'id' => 'testapp',
+            'basePath' => __DIR__,
+            'vendorPath' => $this->getVendorPath(),
+            'components' => [
+                'request' => [
+                    'cookieValidationKey' => 'wefJDF8sfdsfSDefwqdxj9oq',
+                    'scriptFile' => __DIR__ .'/index.php',
+                    'scriptUrl' => '/index.php',
+                ],
+            ]
+        ], $config));
+    }
+
     protected function getVendorPath()
     {
         $vendor = dirname(dirname(__DIR__)) . '/vendor';
@@ -49,6 +65,9 @@ abstract class TestCase extends \PHPUnit_Framework_TestCase
      */
     protected function destroyApplication()
     {
+        if (\Yii::$app && \Yii::$app->has('session', true)) {
+            \Yii::$app->session->close();
+        }
         \Yii::$app = null;
     }
 }
