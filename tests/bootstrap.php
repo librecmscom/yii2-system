2f8ae9d16440a306a704a92e3a39a47468dddba8
xutl <79109681@qq.com>
2016-10-09 18:09:11 +0800

    c

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,14 +1,17 @@
 <?php
 
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'test');
+// ensure we get report on all possible php errors
+error_reporting(-1);
+
+define('YII_ENABLE_ERROR_HANDLER', false);
+define('YII_DEBUG', true);
+
+$_SERVER['SCRIPT_NAME'] = '/' . __DIR__;
+$_SERVER['SCRIPT_FILENAME'] = __FILE__;
 
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 Yii::setAlias('@tests', __DIR__);
 
-new \yii\console\Application([
-    'id' => 'unit',
-    'basePath' => __DIR__,
-]);
+require_once(__DIR__ . '/TestCase.php');
