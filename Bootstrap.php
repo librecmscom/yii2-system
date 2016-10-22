2d868b402bb3f3619b18ee6b18c08520dfe3d27a
xutl <79109681@qq.com>
2016-10-22 10:27:37 +0800

    update

---
+++ Bootstrap.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+
+namespace yuncms\system;
+
+use Yii;
+use yii\web\GroupUrlRule;
+use yii\i18n\PhpMessageSource;
+use yii\base\BootstrapInterface;
+
+/**
+ * Class Bootstrap
+ * @package yuncms/user
+ */
+class Bootstrap implements BootstrapInterface
+{
+    public function bootstrap($app)
+    {
+        
+    }
+}
