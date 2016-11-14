f0169eb43672a133bc703a00911f0a9b99a3613a
xutl <79109681@qq.com>
2016-11-14 13:55:29 +0800

    update

--- src/helpers.php
+++ src/helpers.php
@@ -4,3 +4,11 @@
  * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
  * @license http://www.tintsoft.com/license/
  */
+/**
+ * 将时间戳格式化
+ */
+if(! function_exists('timestamp_format')){
+    function timestamp_format($timestamp){
+        return Yii::$app->formatter->asRelativeTime($timestamp);
+    }
+}
