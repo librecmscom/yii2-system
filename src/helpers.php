63346004ba879d0078f7753a12aea31d3e9a3eb3
xutl <79109681@qq.com>
2016-11-18 15:06:46 +0800

    搞定中文分词

--- src/helpers.php
+++ src/helpers.php
@@ -14,48 +14,65 @@ if (!function_exists('timestamp_format')) {
     }
 }
 
-if (!function_exists('participle')) {
+if (!function_exists('key_extra')) {
+    /**
+     * 从文本中提取关键词
+     * @param string $string
+     * @param int $limit 获取词的数量
+     * @return array
+     */
+    function key_extra($string, $limit = 10)
+    {
+        if (function_exists('scws_new')) {
+            $matches = null;
+            if (preg_match_all("/[a-zA-Z0-9\x{4e00}-\x{9fa5}]+/u", $string, $matches)) {
+                if (isset($matches[0])) {
+                    $string = '';
+                    foreach ($matches[0] as $match) {
+                        $string .= $match;
+                    }
+                    $so = scws_new();
+                    $so->set_charset('utf8');
+                    $so->send_text($string);
+                    $tmp = $so->get_tops($limit);
+                    $so->close();
+                    $words = [];
+                    foreach ($tmp as $key => $value) {
+                        $words[] = $value['word'];
+                    }
+                    return $words;
+                }
+            }
+        }
+        return [];
+    }
+}
+if (!function_exists('pullword')) {
     /**
      * 分词
      * @param string $string
      * @return array 分词结果
      */
-    function participle($string)
+    function pullword($string)
     {
-        if (!function_exists('scws_new')) {
-            return [];
-        }
-        $matches = null;
-        if (preg_match_all('/^[a-zA-Z0-9\x7f-\xff\.@]+$/', $string, $matches)) {
-            if (isset($matches[0])) {
-                $string = '';
-                foreach ($matches[0] as $match) {
-                    $string .= $match;
-                }
-                $so = scws_new();
-                $so->set_charset('utf8');
-                $so->send_text($string);
-                $words = [];
-                while ($tmp = $so->get_result()) {
-                    foreach ($tmp as $k => $v) {
-                        $words = $v['word'];
+        if (function_exists('scws_new')) {
+            $matches = null;
+            if (preg_match_all("/[a-zA-Z0-9\x{4e00}-\x{9fa5}]+/u", $string, $matches)) {
+                if (isset($matches[0])) {
+                    $string = '';
+                    foreach ($matches[0] as $match) {
+                        $string .= $match;
                     }
-                }
-                $so->close();
-                if (!$words) {//分词结果空
-                    return [];
-                }
-                $words = array_unique($words);
-                foreach ($words as $key => $value) {
-                    if (is_null($value) || empty($value) || $value == "的" || $value == "了" || $value == "吗") {
-                        unset ($words[$key]);
+                    $so = scws_new();
+                    $so->set_charset('utf8');
+                    $so->send_text($string);
+                    $words = [];
+                    while ($tmp = $so->get_result()) {
+                        $words = array_merge($words,$tmp);
                     }
+                    $so->close();
+                    return $words;
                 }
-                if (count($words) <= 0) {
-                    return [];
-                }
-
-                return $words;
             }
         }
         return [];
