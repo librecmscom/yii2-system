378438e0ac92cefdd73774e3ec450cfb29d53b02
xuchao <85825770@qq.com>
2017-01-20 18:10:04 +0800

    增加助手函数截取字符

--- src/helpers.php
+++ src/helpers.php
@@ -22,6 +22,30 @@ if (!function_exists('env')) {
     }
 }
 
+
+/**
+ * 提取两个字符串之间的值，不包括分隔符
+ *
+ * @param string $string 待提取的只付出
+ * @param string $start 开始字符串
+ * @param string|null $end 结束字符串，省略将返回所有的。
+ * @return bool string substring between $start and $end or false if either string is not found
+ */
+if (!function_exists('between_substr')) {
+    function between_substr($string, $start, $end = null)
+    {
+        if (($start_pos = strpos($string, $start)) !== false) {
+            if ($end) {
+                if (($end_pos = strpos($string, $end, $start_pos + strlen($start))) !== false) {
+                    return substr($string, $start_pos + strlen($start), $end_pos - ($start_pos + strlen($start)));
+                }
+            } else {
+                return substr($string, $start_pos);
+            }
+        }
+        return false;
+    }
+}
 /**
  * 将时间戳格式化
  */
