a366e28a3f66341c581dbbb0fac7c0cf0875a6a7
xuchao <85825770@qq.com>
2017-02-06 12:22:57 +0800

    update

--- src/widgets/ListGroup.php
+++ src/widgets/ListGroup.php
@@ -13,6 +13,8 @@ use yii\helpers\Url;
 use yii\helpers\Html;
 use yii\bootstrap\Nav;
 use yii\base\InvalidConfigException;
+use yii\bootstrap\Widget;
+use yii\bootstrap\BootstrapAsset;
 
 /**
  * Menu displays a multi-level menu using nested HTML lists.
@@ -47,8 +49,61 @@ use yii\base\InvalidConfigException;
  * @author Qiang Xue <qiang.xue@gmail.com>
  * @since 2.0
  */
-class ListGroup extends Nav
+class ListGroup extends Widget
 {
+    /**
+     * @var array list of items in the nav widget. Each array element represents a single
+     * menu item which can be either a string or an array with the following structure:
+     *
+     * - label: string, required, the nav item label.
+     * - url: optional, the item's URL. Defaults to "#".
+     * - visible: boolean, optional, whether this menu item is visible. Defaults to true.
+     * - linkOptions: array, optional, the HTML attributes of the item's link.
+     * - options: array, optional, the HTML attributes of the item container (LI).
+     * - active: boolean, optional, whether the item should be on active state or not.
+     * - dropDownOptions: array, optional, the HTML options that will passed to the [[Dropdown]] widget.
+     * - items: array|string, optional, the configuration array for creating a [[Dropdown]] widget,
+     *   or a string representing the dropdown menu. Note that Bootstrap does not support sub-dropdown menus.
+     * - encode: boolean, optional, whether the label will be HTML-encoded. If set, supersedes the $encodeLabels option for only this item.
+     *
+     * If a menu item is a string, it will be rendered directly without HTML encoding.
+     */
+    public $items = [];
+    /**
+     * @var boolean whether the nav items labels should be HTML-encoded.
+     */
+    public $encodeLabels = true;
+    /**
+     * @var boolean whether to automatically activate items according to whether their route setting
+     * matches the currently requested route.
+     * @see isItemActive
+     */
+    public $activateItems = true;
+    /**
+     * @var boolean whether to activate parent menu items when one of the corresponding child menu items is active.
+     */
+    public $activateParents = false;
+    /**
+     * @var string the route used to determine if a menu item is active or not.
+     * If not set, it will use the route of the current request.
+     * @see params
+     * @see isItemActive
+     */
+    public $route;
+    /**
+     * @var array the parameters used to determine if a menu item is active or not.
+     * If not set, it will use `$_GET`.
+     * @see route
+     * @see isItemActive
+     */
+    public $params;
+    /**
+     * @var string this property allows you to customize the HTML which is used to generate the drop down caret symbol,
+     * which is displayed next to the button text to indicate the drop down functionality.
+     * Defaults to `null` which means `<b class="caret"></b>` will be used. To disable the caret, set this property to be an empty string.
+     */
+    public $dropDownCaret;
+
     /**
      * @var array list of HTML attributes shared by all menu [[items]]. If any individual menu item
      * specifies its `options`, it will be merged with this property before being used to generate the HTML
@@ -63,6 +118,33 @@ class ListGroup extends Nav
     public $itemOptions = [];
 
     /**
+     * Initializes the widget.
+     */
+    public function init()
+    {
+        parent::init();
+        if ($this->route === null && Yii::$app->controller !== null) {
+            $this->route = Yii::$app->controller->getRoute();
+        }
+        if ($this->params === null) {
+            $this->params = Yii::$app->request->getQueryParams();
+        }
+        if ($this->dropDownCaret === null) {
+            $this->dropDownCaret = Html::tag('b', '', ['class' => 'caret']);
+        }
+    }
+
+    /**
+     * Renders the widget.
+     */
+    public function run()
+    {
+        BootstrapAsset::register($this->getView());
+        return $this->renderItems();
+    }
+
+
+    /**
      * Renders widget items.
      */
     public function renderItems()
@@ -74,8 +156,8 @@ class ListGroup extends Nav
             }
             $items[] = $this->renderItem($item);
         }
-
-        return Html::tag('nav', implode("\n", $items), $this->options);
+        $tag = ArrayHelper::remove($this->options, 'tag', 'nav');
+        return Html::tag($tag, implode("\n", $items), $this->options);
     }
 
     /**
@@ -136,5 +218,42 @@ class ListGroup extends Nav
             }
         }
         return $items;
+    }
+
+    /**
+     * Checks whether a menu item is active.
+     * This is done by checking if [[route]] and [[params]] match that specified in the `url` option of the menu item.
+     * When the `url` option of a menu item is specified in terms of an array, its first element is treated
+     * as the route for the item and the rest of the elements are the associated parameters.
+     * Only when its route and parameters match [[route]] and [[params]], respectively, will a menu item
+     * be considered active.
+     * @param array $item the menu item to be checked
+     * @return boolean whether the menu item is active
+     */
+    protected function isItemActive($item)
+    {
+        if (isset($item['url']) && is_array($item['url']) && isset($item['url'][0])) {
+            $route = $item['url'][0];
+            if ($route[0] !== '/' && Yii::$app->controller) {
+                $route = Yii::$app->controller->module->getUniqueId() . '/' . $route;
+            }
+            if (ltrim($route, '/') !== $this->route) {
+                return false;
+            }
+            unset($item['url']['#']);
+            if (count($item['url']) > 1) {
+                $params = $item['url'];
+                unset($params[0]);
+                foreach ($params as $name => $value) {
+                    if ($value !== null && (!isset($this->params[$name]) || $this->params[$name] != $value)) {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        return false;
     }
 }
