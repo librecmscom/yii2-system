8465ed9a5bab88e63a2f78d87eb86388b13f224e
xuchao <85825770@qq.com>
2017-07-03 11:18:23 +0800

    增加一个 action执行时间的记录器

---
+++ src/filters/ActionTimeFilter.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+namespace yuncms\system\filters;
+
+use Yii;
+use yii\base\ActionFilter;
+
+/**
+ * Class ActionTimeFilter
+ * @package yuncms\system
+ */
+class ActionTimeFilter extends ActionFilter
+{
+    private $_startTime;
+
+    public function beforeAction($action)
+    {
+        $this->_startTime = microtime(true);
+        return parent::beforeAction($action);
+    }
+
+    public function afterAction($action, $result)
+    {
+        $time = microtime(true) - $this->_startTime;
+        Yii::trace("Action '{$action->uniqueId}' spent $time second.");
+        return parent::afterAction($action, $result);
+    }
+}
